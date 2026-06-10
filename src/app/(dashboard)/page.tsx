cb45693da71b539c1661a140d66b1bc29f70a54f
Adil <shaikhmdadil191@gmail.com>
2026-06-10 20:49:37 +0530

    add dashboard layout and sidebar components; remove old home page

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className='flex flex-1 items-center justify-center'>
+        <h1 className="text-2xl font-semibold">
+        Dashboard
+        </h1>
+        
+    </div>
+  )
+}
