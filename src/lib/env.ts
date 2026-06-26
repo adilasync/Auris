37dd4b490e535f5dfec7a2f2270ca0a92275288d
Adil <shaikhmdadil191@gmail.com>
2026-06-26 10:56:54 +0530

    added s3 and trpc

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,3 +1,16 @@
-export const env = {
-  DATABASE_URL: process.env.DATABASE_URL as string,
-};
+import { z } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().min(1),
+    APP_URL: z.string().min(1),
+
+    AWS_REGION: z.string().min(1),
+    AWS_ACCESS_KEY_ID: z.string().min(1),
+    AWS_SECRET_ACCESS_KEY: z.string().min(1),
+    S3_BUCKET_NAME: z.string().min(1),
+  },
+  experimental__runtimeEnv: {},
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+});
